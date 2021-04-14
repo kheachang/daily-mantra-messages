53d81c0a12507961a1296f70b747eca2efc6f5d3
Khea Chang <kheachang@Kheas-MacBook-Air.local>
2021-04-14 15:39:21 -0700

    added pastRandomNumber

--- script.js
+++ script.js
@@ -2,13 +2,18 @@ greeting = ['Good morning!', 'Hello!', 'Howdy!', 'Good day!'];
 message = ['I am supported', 'Things happen for me not to me', 'Resilience is what you tell yourself of reality; how you frame reality', 'Desired traits are learned', 'We attract what we are ready for', 'I am exactly where I should be', 'I am always deserving and worth of what I want'];
 end = ['Have a great day!']
 text = 'Your mantra today is: '
+let pastRandomNumber;
 
 const randomMessageProgram = (greeting, message, end) => {
-  let historyOfRandomNumber = [];
+  
   randomGreeting = Math.floor(Math.random()*(greeting.length));
   randomMessage = Math.floor(Math.random()*(message.length));
+  if(randomMessage === pastRandomNumber){
+    randomMessage = Math.floor(Math.random()*(message.length));
+  }
   randomEnd = Math.floor(Math.random()*(end.length));
   
   return greeting[randomGreeting] + ' ' + text + message[randomMessage] + '.' + ' ' + end[randomEnd];
 }
-//console.log(randomMessageProgram(greeting, message, end));
+console.log(randomMessageProgram(greeting, message, end));
+//console.log(pastRandomNumber);
