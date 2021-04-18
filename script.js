6826412319f898801e80aa14e596516868a3b2ed
Khea Chang <kheachang@Kheas-MacBook-Air.local>
2021-04-17 19:47:59 -0700

    add DOM

--- script.js
+++ script.js
@@ -2,25 +2,21 @@ greeting = ['Good morning!', 'Hello!', 'Howdy!', 'Good day!'];
 message = ['I am supported', 'Things happen for me not to me', 'Resilience is what you tell yourself of reality; how you frame reality', 'Desired traits are learned', 'We attract what we are ready for', 'I am exactly where I should be', 'I am always deserving and worth of what I want'];
 end = ['Have a great day!']
 text = 'Your mantra today is: '
-let pastRandomNumber;
 
 const randomMessageProgram = (greeting, message, end) => {
   
   randomGreeting = Math.floor(Math.random()*(greeting.length));
   randomMessage = Math.floor(Math.random()*(message.length));
-  if(randomMessage === pastRandomNumber){
-    randomMessage = Math.floor(Math.random()*(message.length));
-  }
   randomEnd = Math.floor(Math.random()*(end.length));
   
-  return console.log(greeting[randomGreeting] + ' ' + text + message[randomMessage] + '.' + ' ' + end[randomEnd]);
+  return greeting[randomGreeting] + ' ' + text + message[randomMessage] + '.' + ' ' + end[randomEnd];
 }
 
-//DOM
-let button = document.getElementById('button');
+let button = document.getElementById('button'); 
+let words = document.getElementById('words');
 
 function runProgram(event) {
+  words.innerHTML = randomMessageProgram(greeting, message, end);
+  button.innerHTML = "Come back tomorrow!";
   
 }
-
-button.onclick = runProgram; 
